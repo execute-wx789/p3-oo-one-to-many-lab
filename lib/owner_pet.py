2620738c0a90fc7ba6f5b9c690af425966de265c
Yurick <YJakse789@gmail.com>
2023-09-14 12:32:33 -0400

    Completed Lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,33 @@
 class Pet:
-    pass
+    PET_TYPES = ['dog', 'cat', 'rodent', 'bird', 'reptile', 'exotic']
+    all = []
+    def __init__(self,name,pet_type,owner=None):
+        if pet_type in Pet.PET_TYPES:
+            self.pet_type = pet_type
+        else:
+            raise Exception
+        self.owner = owner
+        self.name = name
+        Pet.all.append(self)
 
-class Owner:
-    pass
+
+class Owner(Pet):
+    def __init__(self,name):
+        self.name = name
+    def pets(self):
+        returnList = []
+        for pet in Pet.all:
+            if pet.owner == self:
+                returnList.append(pet)
+        return returnList
+    def add_pet(self, pet):
+        pet.owner = self
+    def get_sorted_pets(self):
+        returnList = []
+        for pet in Pet.all:
+            if pet.owner == self:
+                returnList.append(pet)
+        def sortKey(e):
+            return e.name
+        returnList.sort(key=sortKey)
+        return returnList
